2679bf96afb19f219fd1e38fc1ca9a83d4c74d47
valeriasilva06 <1ainforvaleriasilva@gmail.com>
2026-07-28 17:12:44 -0500

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 
 # Configuración de la página
